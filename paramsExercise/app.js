e35031ac349c288592ec77f5343bd376fe3417e7
edudroubi <eduardodroubi@gmail.com>
2020-08-06 16:31:24 -0300

    add params exercise

---
+++ paramsExercise/app.js
@@ -0,0 +1,41 @@
+// Import lib
+const express = require("express");
+
+// Ambient vars
+const app = express();
+const port = 3000;
+
+// Set root page
+app.get("/", function (req, res) {
+  res.send("Hi there, welcome to my assignment!");
+});
+
+// Set /speak/animal page
+app.get("/speak/:animal", function (req, res) {
+  let animal = req.params.animal.toLowerCase();
+  let say = {
+    pig: "'Oink'",
+    dog: "'Woof Woof'",
+    cow: "'Moo'"
+  };
+
+  res.send("The " + animal + " says " + say[animal]);
+});
+
+// Set /repeat/str/num page
+app.get("/repeat/:str/:num", function (req, res) {
+  let str = req.params.str;
+  let num = Number(req.params.num);
+  let text = [];
+
+  for (let i = 0; i < num; i++) {
+    text += str + " ";
+  }
+
+  res.send(text);
+});
+
+// Listen for resquests
+app.listen(port, function () {
+  console.log("Server Started at http://localhost:" + port);
+});
